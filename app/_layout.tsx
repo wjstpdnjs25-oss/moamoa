4b6f2362798be14c40bab9ce0ee0fd9339bf0290
DESKTOP-HIJNJCS\user <lili10148-@chungbuk.ac.kr>
2026-05-27 14:46:30 +0900

    feat: implement main screen UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import React from 'react';
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      {children}
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
