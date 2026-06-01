7d541582e6ebd801206801f3442b1c64c938ec5c
DESKTOP-HIJNJCS\user <lili10148-@chungbuk.ac.kr>
2026-06-01 10:25:06 +0900

    feat: calculate total budget automatically

--- app/budget.tsx
+++ app/budget.tsx
@@ -7,7 +7,20 @@ import {
     View,
 } from "react-native";
 
+import { useState } from "react";
+
+
+
 export default function BudgetScreen() {
+    const [food, setFood] = useState("");
+    const [transport, setTransport] = useState("");
+    const [shopping, setShopping] = useState("");
+    const [cafe, setCafe] = useState("");
+    const totalBudget =
+    Number(food || 0) +
+    Number(transport || 0) +
+    Number(shopping || 0) +
+    Number(cafe || 0);
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>예산 설정</Text>
@@ -16,17 +29,40 @@ export default function BudgetScreen() {
       <View style={styles.card}>
         <Text style={styles.label}>카테고리별 예산</Text>
 
-        <TextInput placeholder="식비" style={styles.input} />
-        <TextInput placeholder="교통" style={styles.input} />
-        <TextInput placeholder="쇼핑" style={styles.input} />
-        <TextInput placeholder="카페" style={styles.input} />
+        <TextInput 
+        placeholder="식비" 
+        style={styles.input}
+        keyboardType="numeric"
+        value={food}
+        onChangeText={setFood}
+         />
+        <TextInput 
+        placeholder="교통" 
+        style={styles.input} 
+        keyboardType="numeric"
+        value={transport}
+        onChangeText={setTransport}
+        />
+        <TextInput 
+        placeholder="쇼핑" 
+        style={styles.input} 
+        keyboardType="numeric"
+        value={shopping}
+        onChangeText={setShopping}
+        />
+        <TextInput 
+        placeholder="카페" 
+        style={styles.input} 
+        keyboardType="numeric"
+        value={cafe}
+        onChangeText={setCafe}/>
       </View>
 
       <View style={styles.card}>
         <Text style={styles.label}>총 예산</Text>
 
          <Text style={styles.totalBudgetText}>
-         ₩ 0
+         ₩ {totalBudget.toLocaleString()}
      </Text>
     </View>
 
